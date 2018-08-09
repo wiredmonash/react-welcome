783ca66f444f8396431028a4653a6562421e9efc
Nicholas Whittaker <nchlswhttkr@gmail.com>
2018-08-09 18:30:51 +1000

    Added cool new features

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from "./wired.svg";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to WIRED</h1>
+        <header>
+          <img src={logo} alt="logo" />
+          <h1>Welcome to WIRED</h1>
         </header>
-        <p className="App-intro">
-          This page is under development, PRs are welcome on{" "}
-          <a href="https://github.com/wiredmonash/react-welcome">GitHub</a>.
+        <p className="foreword">
+          <marquee>
+            WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED
+            WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED WIRED
+          </marquee>
+        </p>
+
+        <p className="foreword">
+          WIRED Monash is the student club for those studying IT or those who
+          are curious about everything digital at Monash University Clayton.
+        </p>
+
+        <p>
+          We aim to provide a relaxed, social atmosphere where students can meet
+          others who share their love for everything digital. Our community is
+          one where we can share our opinions on the latest technology, and
+          collaborate on projects and competitions.
+        </p>
+
+        <p>
+          We also provide our members the opportunity to connect with some of
+          Australia's leading technology companies through our many sponsored
+          lunchtime events and networking opportunities - such as our Industry
+          Night.
+        </p>
+
+        <p>
+          Past events that we have held include our Annual Trivia Night,
+          Midnight-to-Dawn Laser Tag, and the hugely successful UNIHACK
+          Melbourne hackathon competition. We also hold workshops to allow
+          students to expand their knowledge such as data visualisation and
+          cyber security.
         </p>
       </div>
     );
